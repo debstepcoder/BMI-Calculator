54f3dbef782445e67c51a791689d209ead4c4079
debstepcoder <deblin.kumar@gmail.com>
2023-08-20 11:34:37 +0530

    removed comment

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// const form = document.querySelector('form');
+const form = document.querySelector('form');
 
 // document.querySelector('form').addEventListener('submit', (e) => {
 //     e.preventDefault()
